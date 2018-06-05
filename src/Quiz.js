0477757944b282708f18c9c0873e13ab068358a9
Eno Bassey <ennyboy@gmail.com>
2018-06-06 00:36:56 +0100

    correct Quiz file
    correction made

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends React.Component {
 
   render(){
     return <div>
-        <div className="QuizQuestion">{quizData.quiz_questions[this.state.quiz_position].instruction_text}</div>
+        <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
     </div>;
   }
 }
