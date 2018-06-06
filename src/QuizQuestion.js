11347268cb0a43e0c6eba1c8270c8e385246dffc
Eno Bassey <ennyboy@gmail.com>
2018-06-06 15:42:39 +0100

    pass props

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 class QuizQuestion extends Component {
   constructor(props){
-    super()
+    super(props)
     this.state = {
       incorrectAnswer: false
     }
