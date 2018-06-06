627929f404586508a9f45e7c2bfe5960d03330f7
Eno Bassey <ennyboy@gmail.com>
2018-06-06 12:29:32 +0100

    s

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,12 +15,12 @@ class Quiz extends Component {
   render(){
     const isQuizEnd =( this.state.quiz_position-1 ===quizData.quiz_questions.length) ;
     return <div>
-    if(!isQuizEnd){
+    if(isQuizEnd){
         <QuizEnd />
-    }
+    } else {
 
         <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}/>
-    </div>;
+  }  </div>;
   }
 }
 
